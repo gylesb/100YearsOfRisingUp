7c430c1808d3914ec92abe563e6a67a191364a6c
Gyles Batara <gyles.batara@live.com>
2018-03-07 10:35:37 -0800

    Add App component and hardcode a couple of examples onto home page

--- src/components/ConfirmationQuestions.jsx
+++ src/components/ConfirmationQuestions.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+function ConfirmationQuestions(props){
+  return (
+    <div>
+      <p>Are you sure you would like to Insert a new club?</p>
+      <button onClick = {props.onNewClubConfirmation}>Yes</button>
+    </div>
+  );
+}
+
+ConfirmationQuestions.propTypes = {
+  onNewClubConfirmation: PropTypes.func
+};
+
+export default ConfirmationQuestions;
--- src/components/NewClubControl.jsx
+++ src/components/NewClubControl.jsx
@@ -1,1 +1,39 @@
 import React from 'react';
+import NewClubForm from './NewClubForm';
+import PropTypes from 'prop-types';
+import ConfirmationQuestions from './ConfirmationQuestions';
+
+class NewClubControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+    this.handleNewClubConfirmation = this.handleNewClubConfirmation.bind(this);
+  }
+
+  handleNewClubConfirmation() {
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render() {
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleContent = <NewClubForm onNewClubCreation = {this.props.onNewClubCreation}/>;
+    } else { currentlyVisibleContent = <ConfirmationQuestions onNewClubConfirmation =    {this.handleNewClubConfirmation}/>;
+    }
+
+    return (
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
+}
+
+NewClubControl.propTypes = {
+  onNewClubCreation: PropTypes.func
+};
+
+export default NewClubControl;
