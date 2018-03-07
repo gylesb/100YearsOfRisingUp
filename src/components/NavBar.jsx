10c8bcedc549dbfcdafadaee847ab85929ce7860
Gyles Batara <gyles.batara@live.com>
2018-03-07 10:38:17 -0800

    Add NavBar, Header, Error404 functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,1 +1,25 @@
 import React from 'react';
+
+
+function NavBar(){
+  return (
+    <div className="row" style={{
+      padding: '10px',
+      textAlign: 'center',
+      backgroundColor: '#3CC6FF',
+      borderRadius: '20px'
+    }}>
+      <div className="col-md-4">
+        <a href='/#/'>Home</a>
+      </div>
+      <div className="col-md-4">
+        <a href='/#/newclub'>Add Club</a>
+      </div>
+      <div className="col-md-4">
+        <a href='/#/about'>About</a>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
